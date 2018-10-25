3eb40c3f2de218cd25fcdae3f953a88a89616b90
andrerodrigues0018 <andre.rodrigues0018@gmail.com>
2018-10-25 14:55:06 -0300

    comentários das alterações 2

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
-<?php 
-echo "Oi";
-?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Teste</title>
 </head>
 <body>
+	<style type="text/css">
+		*{
+			float: left;
+			margin:0;
+
+		}
+		.boxLink{
+			background: #333;
+			width: 300px;
+			color: #ccc;
+			margin:40px;
+			height: 300px;
+		}
+	</style>
+	<div class="boxLink">
+		<?php 
+			echo "Oi";
+		?>
+
+	</div>
 
 </body>
 </html>
